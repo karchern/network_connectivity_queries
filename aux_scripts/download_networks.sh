f067a6bc61be42f59010e61e238f36e9e8591da3
Nicolai Karcher <karchernic@gmail.com>
2025-03-06 14:02:09 +0100

    move some files

--- aux_scripts/download_networks.sh
+++ aux_scripts/download_networks.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -e
 base=$1
-out="networks/"
+out="../networks/"
 mkdir -p $out
 while read line
 do
-	# Skip the line if it doesnt start with a number
+	# Skip the line if it doesnt start with a number (these are comment lines)
     if ! echo "$line" | grep -q '^[0-9]'; then
         continue
     fi	
-
 	networkid=$(echo $line | awk '{print $1}')
 	networkname=$(echo $line | awk '{print $2}')
 	wget http://rest.kegg.jp/get/${base}${networkid}/kgml -O ${out}${base}${networkid}.xml
-done < network_ids_to_operate_on.txt
+done < ../network_ids_to_operate_on.txt
---
+++ aux_scripts/print_cytoscape_commands.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+set -euo pipefail
+
+rm -f cytoscape_command_files/*
+
+base="bvu"
+while read line
+do
+	# Skip the line if it doesnt start with a number (these are comment lines)
+    if ! echo "$line" | grep -q '^[0-9]'; then
+        continue
+    fi	
+	networkid=$(echo $line | awk '{print $1}')
+	# Save the rest of the white space delimited lines as networkname
+	networkname=$(echo $line | awk '{for(i=2;i<=NF;++i)printf "%s ", $i; print ""}')
+	echo -e "network import file file=\"$(pwd)/../networks/${base}${networkid}.xml\""
+	echo -e "network delete network=\"${networkname} [${base}${networkid}]\" nodeList=KEGG_NODE_TYPE:map"
+done < ../network_ids_to_operate_on.txt > cytoscape_command_files/tmp.txt
+
+n=10
+cd cytoscape_command_files
+
+# Split tmp.txt into files with n lines
+split -l $n tmp.txt split_
+
+# Rename the split files to have a numeric suffix
+a=1
+for f in split_*
+do
+    mv "$f" "split_${a}.txt"
+    a=$((a + 1))
+done
+
+for f in $(ls . | grep txt$ | sed "s/\.txt//")
+do
+	cat ${f}.txt <(echo -e "session save as file=\"$(pwd)/../cytoscape_session_files/${f}.cys\"\ncommand quit") > a
+	mv a ${f}.txt
+done
+
+rm -f a
+rm -f tmp.txt
+cd ..
