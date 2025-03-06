e364213fb0088aede7b8b515b21f0956abdd7212
Nicolai Karcher <karchernic@gmail.com>
2025-03-06 14:16:22 +0100

    removing query_network.sh

--- aux_scripts/query_network.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-set -euo pipefail
-python /Users/karcher/network_connectivity_queries/network_queries/main.py --ko_metabolite_csv /Users/karcher/network_connectivity_queries/ko_metabolite_map.csv
